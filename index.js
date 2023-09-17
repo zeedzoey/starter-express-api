6291fa097d62e31d5460e055b412197cb34a20d8
zeedzoey <145294465+zeedzoey@users.noreply.github.com>
2023-09-17 14:39:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Serve static files from the "Tip_Coin_files" directory
+app.use(express.static(path.join(__dirname, "")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "thetipcoin.html"));
+});
+
 app.listen(process.env.PORT || 3000)
+
